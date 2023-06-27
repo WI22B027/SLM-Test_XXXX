53bde00b698adc8db6c8cc6b7606c271b9294d15
WI22B027 <wi22b027@technikum-wien.at>
2023-06-27 13:47:34 +0200

    Initial commit

---
+++ src/test/java/at/technikum/SlmTestXxxApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmTestXxxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
