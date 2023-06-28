26711746e0022ad26bc68e484fa06d524fd47227
WI22B027 <wi22b027@technikum-wien.at>
2023-06-28 18:39:08 +0200

    add the feature factorials with test

--- src/test/java/at/technikum/SlmTestXxxApplicationTests.java
+++ src/test/java/at/technikum/SlmTestXxxApplicationTests.java
@@ -1,18 +1,26 @@
 package at.technikum;
 
+import at.technikum.controller.SlmTestXxxxController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SlmTestXxxApplicationTests {
 
     @Test
-    void nameTest() {
+    void getFactTest() {
         //Arrange
+        int result = 120;
+        SlmTestXxxxController mc = new SlmTestXxxxController();
 
         //Act
+        int factorial = mc.getFactorial(5);
+
 
         //Assert
+        assertEquals(factorial, result);
     }
 
 }
